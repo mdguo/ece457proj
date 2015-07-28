1ff95ce6d616f0d95f556a0af3ec1d40a90a0f0f
Michael Guo <mikeguo1992@gmail.com>
2015-07-28 19:35:18 -0400

    single ant solution

--- AntColonySystem.m
+++ AntColonySystem.m
@@ -1,4 +1,4 @@
-function [hValues] = AntColonySystem(iniCoords, iniSol, paths, cellAdjacencies, startingAdjacencies)
+function [hValues] = AntColonySystem(iniCoords, goalCoord, iniSol, paths, cellAdjacencies, startingAdjacencies)
     % final return value is a set of hValues equal to number of paths
     % hValues = rand(nnz(paths),1);
     
@@ -24,21 +24,20 @@ function [hValues] = AntColonySystem(iniCoords, iniSol, paths, cellAdjacencies, 
         pathVector(:,i) = lob + (upb-lob) .* rand(numChoices, 1);
     end
     
-    phmone
-    pathVector
+    %phmone
+    %pathVector
     
+    % 3 stages
+    % starAdja, path(1)    
+    % path(2) ~ path(end)    
+    % path(end), goalCoord
+    
+    % first stage, from iniCoord to path(1)
     currPos = iniCoords(1,:);
-    
-    [boundary, distVector] = calculateDistanceVector(numChoices, currPos, pathVector(:,1), paths, cellAdjacencies, startingAdjacencies);
-    boundary
-    distVector
-    
-    decisionVec = zeros(numChoices, 1);
-    for pathNum = 1:numChoices
-        decisionVec(pathNum) = calcProbability(pathNum, distVector, alpha, phmone(:,1), beta);
-    end
-    
-    decisionVec
+    fromCell = startingAdjacencies(1);
+    toCell = paths(1);
+    [boundary, distVector] = calcDistanceVector(numChoices, currPos, pathVector(:,1), cellAdjacencies, fromCell, toCell);    
+    decisionVec = calcDecisionVector(numChoices, distVector, alpha, phmone(:,1), beta);
     
     % choose a node to take and update the current position
     [maxProb, maxIndex] = max(decisionVec);
@@ -46,18 +45,42 @@ function [hValues] = AntColonySystem(iniCoords, iniSol, paths, cellAdjacencies, 
     
     chosen = pathVector(maxIndex, 1);
     hValues(1) = chosen;
+    
+    % stage 2, from path(1) to path(end)
     currPos = updateCurrPos(boundary, chosen);
     
+    for j = 1:size(paths, 2)-1
+        fromCell = paths(j);
+        toCell = paths(j+1);
+        [boundary, distVector] = calcDistanceVector(numChoices, currPos, pathVector(:,j), cellAdjacencies, fromCell, toCell);
+        decisionVec = calcDecisionVector(numChoices, distVector, alpha, phmone(:,j), beta);
+        [maxProb, maxIndex] = max(decisionVec);
+        chosen = pathVector(maxIndex, j);
+        hValues(j+1) = chosen;
+        currPos = updateCurrPos(boundary, chosen);
+    end
+    
+    % stage 3, from path(end) to goalCoord
+    % no need to implement, just go straight from currPos to goalCoord
+    
+    hValues
     
 end
 
-function[pos] = updateCurrPos(boundary, chosen)
+function [pos] = updateCurrPos(boundary, chosen)
     pos(1) = boundary(1);
     pos(2) = boundary(2) + (boundary(3) - boundary(2)) * chosen;
 end
 
-function [boundary, distVec] = calculateDistanceVector(numChoices, fromCoord, nodeHeights, paths, cellAdjacencies, startingAdjacencies)
-    boundary = cellAdjacencies(:,startingAdjacencies(1),paths(1));
+function [decisionVec] = calcDecisionVector(numChoices, distVector, alpha, phmoneVal, beta)
+    decisionVec = zeros(numChoices, 1);
+    for pathNum = 1:numChoices
+        decisionVec(pathNum) = calcProbability(pathNum, distVector, alpha, phmoneVal, beta);
+    end
+end
+
+function [boundary, distVec] = calcDistanceVector(numChoices, fromCoord, nodeHeights, cellAdjacencies, fromCell, toCell)
+    boundary = cellAdjacencies(:, fromCell, toCell);
     from = fromCoord;
     distVec = zeros(numChoices, 1);
     
